ff97fd273f525b11c0f63709ffed50f15034ecc2
smartestnerdfromtown <188559703+smartestnerdfromtown@users.noreply.github.com>
2026-02-11 12:17:54 +0400

    dumbest chess engine I've ever seen

--- main.py
+++ main.py
@@ -30,6 +30,79 @@ piece_values = {
     chess.KING: 0
 }
 
+# -------------------------
+# Simple Engine (1-ply)
+# -------------------------
+def evaluate(board: chess.Board):
+    if board.is_checkmate:
+        if board.turn:
+            return -999999
+        else:
+            return 999999
+    
+    score = 0
+
+    for piece_type in piece_values:
+        score += len(board.pieces(piece_type, chess.WHITE)) * piece_values[piece_type]
+        score -= len(board.pieces(piece_type, chess.BLACK)) * piece_values[piece_type]
+
+    return score
+
+def minimax(depth, alpha, beta, maximizing):
+    if depth == 0 or board.is_game_over():
+        return evaluate(board)
+
+    if maximizing:
+        max_eval = -999999
+        for move in board.legal_moves:
+            board.push(move)
+            eval = minimax(depth-1, alpha, beta, False)
+            board.pop()
+
+            max_eval = max(max_eval, eval)
+            alpha = max(alpha, eval)
+            if beta <= alpha:
+                break
+
+        return max_eval
+    else:
+        min_eval = 999999
+        for move in board.legal_moves:
+            board.push(move)
+            eval = minimax(depth-1, alpha, beta, True)
+            board.pop()
+
+            min_eval = min(min_eval, eval)
+            beta = min(beta, eval)
+            if beta <= alpha:
+                break
+
+        return min_eval
+
+def engine_move(depth=3):
+    best_move = None
+    best_value = -9999 if board.turn == chess.WHITE else 9999
+
+    for move in board.legal_moves:
+        board.push(move)
+        value = minimax(depth-1, -999999, 999999, board.turn == chess.WHITE)
+        board.pop()
+
+        if board.turn == chess.WHITE:
+            if value > best_value:
+                best_value = value
+                best_move = move
+        else:
+            if value < best_value:
+                best_value = value
+                best_move = move
+
+    return best_move
+
+
+# -------------------------
+# Drawing
+# -------------------------
 def load_images(square_size):
     images = {}
 
@@ -58,43 +131,6 @@ def load_images(square_size):
 IMAGES = load_images(SQ)
 pprint.pprint(IMAGES)
 
-# -------------------------
-# Simple Engine (1-ply)
-# -------------------------
-def evaluate(b):
-    score = 0
-    for piece_type in piece_values:
-        score += len(b.pieces(piece_type, chess.WHITE)) * piece_values[piece_type]
-        score -= len(b.pieces(piece_type, chess.BLACK)) * piece_values[piece_type]
-    return score
-
-def engine_move():
-    best_move = None
-    best_score = -9999 if board.turn == chess.WHITE else 9999
-
-    for move in board.legal_moves:
-        print(list(board.legal_moves))
-        random_move = random.choice(list(board.legal_moves))
-        board.push(move)
-        score = evaluate(board)
-        board.pop()
-
-        if board.turn == chess.WHITE:
-            if score > best_score:
-                best_score = score
-                best_move = move
-        else:
-            if score < best_score:
-                best_score = score
-                best_move = move
-
-    print(best_move)
-    return best_move if best_move else random_move
-
-
-# -------------------------
-# Drawing
-# -------------------------
 def draw_coordinates():
     files = "abcdefgh"
     ranks = "12345678"
@@ -160,7 +196,7 @@ while True:
 
     # Engine plays black
     if board.turn == chess.BLACK:
-        move = engine_move()
+        move = engine_move(depth=3)
         board.push(move)
 
     for event in pygame.event.get():
